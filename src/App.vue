3d80ee5f7190ea48c58d60e6b399b7fe118882f0
hilmysyarif <hilmysyarif@gmail.com>
2020-10-15 00:28:56 +0700

    2 files modified

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,30 @@
 <template>
-  <div id="activityPage">
-    <!--div>
-            <b-navbar toggleable="lg" type="dark" variant="info">
-                <b-navbar-brand href="#">Greedy Bets</b-navbar-brand>
-            </b-navbar>
-        </div-->
-    <router-view></router-view>
+  <div id="app">
+    <div id="activityPage">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import BetIndex from "./components/BetIndex.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
